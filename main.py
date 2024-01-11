4c273c28f2a0426b787eeb531f660df0900670f1
tofi1130 <taafuuu@gmail.com>
2024-01-11 17:07:26 +0500

    Project complete

--- main.py
+++ main.py
@@ -5,7 +5,9 @@ import flet as ft
 def main(page: ft.Page):
     page.title = "Insta people not following you back"
 
-    page.vertical_alignment = ft.MainAxisAlignment.CENTER
+    page.theme = ft.Theme(color_scheme_seed='#01666f')
+
+    # page.vertical_alignment = ft.MainAxisAlignment.CENTER
     page.horizontal_alignment = ft.CrossAxisAlignment.CENTER
 
     page.add(InstFnotFYouApp())
--- ui_components/instfnotfyouapp.py
+++ ui_components/instfnotfyouapp.py
@@ -6,12 +6,15 @@ class InstFnotFYouApp(ft.Column):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
         self.file_picker = ft.FilePicker(on_result=self._get_file_name_from_dialog)
-        self.add_file_button = ft.IconButton(icon=ft.icons.FILE_OPEN, on_click=self.get_file_name)
-        self.submit_button = ft.IconButton(icon=ft.icons.CHECK_SHARP, on_click=self.show_usernames)
-        self.file_path = None
+        self.add_file_button = ft.TextButton(icon=ft.icons.FILE_OPEN, on_click=self.get_file_name)
+        self.submit_button = ft.TextButton(icon=ft.icons.CHECK_SHARP, on_click=self.show_usernames)
 
-        self.controls = [ft.Row([self.add_file_button, self.submit_button], alignment=ft.MainAxisAlignment.CENTER)]
-        self.alignment = ft.MainAxisAlignment.CENTER
+        self.file_path = None
+        self.file_path_field = ft.TextField(hint_text='File Path', disabled=True)
+        
+        self.controls = [ft.Row([self.file_path_field, self.add_file_button], alignment=ft.MainAxisAlignment.CENTER), self.submit_button]
+        
+        self.alignment = ft.MainAxisAlignment.START
         self.horizontal_alignment = ft.CrossAxisAlignment.CENTER
     
     def get_file_name(self, e):
@@ -21,24 +24,19 @@ class InstFnotFYouApp(ft.Column):
     
     def _get_file_name_from_dialog(self, e: ft.FilePickerResultEvent):
         self.file_path = e.files[0].path  # We will work on only one file
-    
-    # @staticmethod
-    # def _get_max_len_in_items(flet_text_controls_list: list[ft.Text]) -> int:
-    #     max_ = len(flet_text_controls_list[0].value)
-    #     for text_control in flet_text_controls_list:
-    #         value_length = len(text_control.value)
-    #         if value_length > max_:
-    #             max_ = value_length
-    #     return max_
+        self.file_path_field.value = self.file_path
+        self.update()
     
     def show_usernames(self, e):
         controls = get_info_flet_controls(self.file_path)
         self.controls.append(
-            ft.DataTable(
-                columns=[ft.DataColumn(ft.Text('People not Following you back'))],
-                rows=[ft.DataRow([ft.DataCell(i)]) for i in controls],
-                # border=ft.border.all()
-            )
-        )
-        print(self.controls)
+            ft.Container(
+                ft.ListView([
+                    ft.DataTable(
+                        columns=[ft.DataColumn(ft.Text('People not Following you back'))],
+                        rows=[ft.DataRow([ft.DataCell(i)]) for i in controls])
+                    ],
+                height=300, width=400),
+            border=ft.border.all(width=1, color=self.page.theme.color_scheme_seed)
+        ))
         self.update()
